2f0c9c502810e976a87bbe18e74a9201025fc0ee
Ubuntu <ubuntu@ip-172-31-18-65.us-west-2.compute.internal>
2019-07-01 05:31:25 +0000

    script update

--- files/user_data.sh
+++ files/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-2 set -e -x
-3 wget -O splunk-7.1.1-8f0ead9ec3db-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.1&product=splunk&filename=splunk-7.1.1-    8f0ead9ec3db-linux-2.6-x86_64.rpm&wget=true'
-4 rpm -i  splunk-7.1.1-8f0ead9ec3db-linux-2.6-x86_64.rpm
-5 sleep 30
-6 sudo -u splunk /opt/splunk/bin/splunk start --answer-yes --no-prompt --accept-license --seed-passwd newpassword
+set -e -x
+wget -O splunk-7.1.1-8f0ead9ec3db-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.1&product=splunk&filename=splunk-7.1.1-    8f0ead9ec3db-linux-2.6-x86_64.rpm&wget=true'
+rpm -i  splunk-7.1.1-8f0ead9ec3db-linux-2.6-x86_64.rpm
+sleep 30
+sudo -u splunk /opt/splunk/bin/splunk start --answer-yes --no-prompt --accept-license --seed-passwd newpassword
