d85f500c0f69c64a4d35579903dee3b5ca327885
Ubuntu <ubuntu@ip-172-31-18-65.us-west-2.compute.internal>
2019-07-02 04:27:54 +0000

    just stable built

--- files/user_data.sh
+++ files/user_data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e -x
-wget -O splunk-7.2.4.2-fb30470262e3-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.4.2&product=splunk&filename=splunk-7.2.4.2-fb30470262e3-linux-2.6-x86_64.rpm&wget=true'
-rpm_path=$(pwd)
+wget -O /tmp/splunk-7.2.4.2-fb30470262e3-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.4.2&product=splunk&filename=splunk-7.2.4.2-fb30470262e3-linux-2.6-x86_64.rpm&wget=true'
+rpm_path="/tmp/splunk-7.2.4.2-fb30470262e3-linux-2.6-x86_64.rpm"
 echo $rpm_path
-rpm_path="$rpm_path/splunk-7.2.4.2-fb30470262e3-linux-2.6-x86_64.rpm"
-echo $rpm_path
+#sudo apt install rpm
 sudo /usr/bin/rpm -ivh $rpm_path --nodeps
 sleep 30
 sudo -u splunk /opt/splunk/bin/splunk start --answer-yes --no-prompt --accept-license --seed-passwd newpassword
